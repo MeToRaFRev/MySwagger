6b1ca06f9c944b68525203f6d41bed419bfa83af
MeToRaFRev <metorafrev@gmail.com>
2022-03-13 23:37:53 +0200

    convert-print

--- routes/convert.js
+++ routes/convert.js
@@ -2,7 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Convertion API')
+    res.json({ api: 'Convertion', paths: [`GET-${req.originalUrl}/swagger`, `GET-${req.originalUrl}/schema`] })
+});
+router.get('/swagger', (req, res) => {
+    res.json({ api: 'Swagger Convertion', paths: [`POST-${req.originalUrl}/v2tov3`, `POST-${req.originalUrl}/v3tov2`, `GET-${req.originalUrl}/v2`, `GET-${req.originalUrl}/v3`] })
+});
+router.get('/schema', (req, res) => {
+    //fill this later
+    res.json({ api: 'Schema Convertion', paths: [`GET-${req.originalUrl}/schema`] })
 });
 
 module.exports = router;
