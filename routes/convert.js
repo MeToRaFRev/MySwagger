5eb2da4503cc582306fd632c11b83b8c50806f47
MeToRaFRev <metorafrev@gmail.com>
2022-03-13 23:12:36 +0200

    Convertion Test

--- routes/convert.js
+++ routes/convert.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json({ "TEST": "TEST" })
+    res.send('Convertion API')
 });
 
 module.exports = router;
