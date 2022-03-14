e8d93bb4060894d1aaba8ca265eb7482d987555a
MeToRaFRev <metorafrev@gmail.com>
2022-03-14 11:41:28 +0200

    Swagger/v2/ToJSV error validation

--- routes/convert.js
+++ routes/convert.js
@@ -71,7 +71,53 @@ router.get("/swagger/v2", (req, res) => {
   });
 });
 
-router.post("/swagger/v2/toJSV", (req, res) => {});
+router.post("/swagger/v2/toJSV", (req, res) => {
+  if (!req.body.swagger) {
+    return res.json({
+      error: "input is not swagger v2",
+      info: "check your body",
+    });
+  }
+  if (!req.header("MySwagger-Path")) {
+    return res.json({
+      error: "no path provided",
+      info: "check your header {MySwagger-Path}",
+    });
+  }
+  if (!req.header("MySwagger-Method")) {
+    return res.json({
+      error: "no method provided",
+      info: "check your header {MySwagger-Method}",
+    });
+  }
+  if (!req.header("MySwagger-Direction")) {
+    return res.json({
+      error: "no direction provided",
+      info: "check your header {MySwagger-Direction}",
+    });
+  }
+  const path = req.header("MySwagger-Path");
+  const method = req.header("MySwagger-Method");
+  const direction = req.header("MySwagger-Direction");
+  if (!req.body.paths[path]) {
+    return res.json({
+      error: "path not found",
+      info: `check your swagger if it has this ${path}`,
+    });
+  }
+  if (!req.body.paths[path][method]) {
+    return res.json({
+      error: "method not found",
+      info: `check your swagger if in ${path} it has this ${method}`,
+    });
+  }
+  if (!direction === "request" || "response") {
+    return res.json({
+      error: "direction is not valid",
+      info: `{MySwagger-Direction} can only be request or response`,
+    });
+  }
+});
 
 router.get("/schema", (req, res) => {
   //fill this later
